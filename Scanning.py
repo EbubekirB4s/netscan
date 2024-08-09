eb3e1f6e410735f4e73545109e0551d04bf81e48
Ebubekir BAŞ <136103560+EbubekirB4s@users.noreply.github.com>
2024-08-09 23:16:54 +0300

    Update Scanning.py#1

--- Scanning.py
+++ Scanning.py
@@ -1,7 +1,16 @@
-def main():
-   return "a"
+import socket
+import sys
+try:  
+    def normalScan(ip):
 
-def udpScan():
-    print("udp scanning")
-
-main()
+        for port in range(130,140):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(3)
+            result = s.connect_ex((ip,port))
+            if(result==0):
+                print('port is opened', port)
+           
+            s.close()
+except socket.error as e:
+    print(e)
+    sys.exit()
