9f32dd0ea115a5c032cac222a32c353a884d3e5c
Ebubekir BAŞ <136103560+EbubekirB4s@users.noreply.github.com>
2024-08-09 23:40:10 +0300

    Update Scanning.py

--- Scanning.py
+++ Scanning.py
@@ -3,10 +3,10 @@ import sys
 try:  
     def normalScan(ip):
 
-        for port in range(130,140):
+        for port in range(134,136):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(3)
-            result = s.connect_ex((ip,port))
+            result = s.connect_ex((socket.gethostbyname(ip),port))
             if(result==0):
                 print('port is opened', port)
            
