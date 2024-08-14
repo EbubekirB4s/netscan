08abbfe561c5edca9e3ba82c53a72c0b2a040185
Ebubekir BAŞ <136103560+EbubekirB4s@users.noreply.github.com>
2024-08-14 23:59:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def argControl():
         print("please enter the Ip")
     else:
         controlIpAdress(args=args)
+        
         # for i in range(0,len(args)):
         #     if('.' in args[i]):
         #         print(args[i])
@@ -31,10 +32,34 @@ def controlIpAdress(args):
             break
         except:
             ip=''
+        
     if(ip==''):
         print('please enter the valid ip adress')
+        exit()
     else:
-        Scanning.normalScan(str(ip))
+        # print(switches.switchdict)
+        
+        catchArgsAndControl(args,str(ip))
+        
+def catchArgsAndControl(args,ip):
+    args.remove(ip)
+    # print(args)
+
+    for i in range(0,len(args)):
+           
+            
+            for x in switches.switchdict.keys():
+                if x in args[i]:
+                        switch=args[i]
+                        switch=switch.replace(x,"")
+                        # print(switch)
+                        if(switch==""):
+                            print("most common ports scanning.")
+                            Scanning.normalScan(str(ip))
+                        else:
+                            print("success")
+                            Scanning.tdpScan(str(ip), int(switch))
+            
         
 
 main()
