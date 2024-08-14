84c464d875eb538997aac71251ff2b524b705c4d
Ebubekir BAŞ <136103560+EbubekirB4s@users.noreply.github.com>
2024-08-14 23:59:57 +0300

    Update Scanning.py

--- Scanning.py
+++ Scanning.py
@@ -1,16 +1,32 @@
 import socket
 import sys
-try:  
-    def normalScan(ip):
+import mostCommonPorts as mcp
 
-        for port in range(134,136):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(3)
-            result = s.connect_ex((socket.gethostbyname(ip),port))
-            if(result==0):
-                print('port is opened', port)
-           
-            s.close()
+try:  
+    def createSocketStream(streamType,timeOutSecond,ip,port):
+        s = socket.socket(socket.AF_INET, streamType)
+        socket.setdefaulttimeout(timeOutSecond)
+        res = s.connect_ex((ip,port))
+        
+        if(res==0):
+            print('port is opened', port)
+        # else:
+        #     print('port is not oppened', port)  
+        s.close()
+    def normalScan(ip):
+        
+        portList = mcp.mostCommonTcpPorts()
+        for i in range(0,len(portList)):
+            createSocketStream(socket.SOCK_STREAM,3,ip,portList[i])
+
+        
+    def tdpScan(ip,port):
+        
+         createSocketStream(socket.SOCK_STREAM,3,ip,145)
+        
+    def udpScan(ip,port):
+        createSocketStream(socket.SOCK_DGRAM, 3,ip,port)
+
 except socket.error as e:
     print(e)
-    sys.exit()
+    sys.exit()()
